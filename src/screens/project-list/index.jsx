b722e488aaf4089a3441e479b5c311edc97ae206
huangguian <841428324@qq.com>
2022-03-06 23:21:52 +0800

    feat: useMount和useDebounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import qs from "qs";
 import { List } from "./list";
 import { SearchPanel } from "./search-panel";
-import { cleanObject } from "../../utils/index";
+import { cleanObject, useMount, useDebounce } from "../../utils/index";
 const url = process.env.REACT_APP_API_URL;
 
 export const ProjectList = () => {
@@ -13,27 +13,25 @@ export const ProjectList = () => {
 
   const [userList, setUserList] = useState([]);
   const [projectsList, setProjectsList] = useState([]);
+  const debounceParams = useDebounce(params, 500);
+
+  useMount(() => {
+    fetch(`${url}/users`).then(async (res) => {
+      if (res.status === 200) {
+        setUserList(await res.json());
+      }
+    });
+  });
 
   useEffect(() => {
-    fetch(`${url}/users`)
-      .then(async (res) => {
-        if (res.status === 200) {
-          setUserList(await res.json());
-        }
-      })
-      .catch();
-  }, []);
-
-  useEffect(() => {
-    const result = cleanObject(params);
-    fetch(
-      `${url}/projects?${qs.stringify(result)}`
-    ).then(async (res) => {
+    const result = cleanObject(debounceParams);
+    console.error("调用接口查询", debounceParams);
+    fetch(`${url}/projects?${qs.stringify(result)}`).then(async (res) => {
       if (res.status === 200) {
         setProjectsList(await res.json());
       }
     });
-  }, [params]);
+  }, [debounceParams]);
 
   return (
     <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => {
   return value === 0 ? false : !value;
 };
@@ -10,4 +12,40 @@ export const cleanObject = (object) => {
     }
   }
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+// export const debounce = (callback, delay) => {
+//   let time = null;
+//   return (...args) => {
+//     if (time) {
+//       clearTimeout(time);
+//     }
+
+//     time = setTimeout(() => {
+//       callback(...args);
+//     }, delay);
+//   };
+// };
+
+export const useDebounce = (value, delay) => {
+  // 状态（响应式：页面跟着更改）
+  const [debounceValue, setDebounceValue] = useState(value);
+  useEffect(() => {
+    const time = setTimeout(() => {
+      console.error("debounce---");
+      setDebounceValue(value);
+    }, delay);
+
+    // 每次在上一个useEffect处理完以后再运行
+    return () => {
+      clearTimeout(time);
+    };
+  }, [value, delay]);
+  return debounceValue;
 };
