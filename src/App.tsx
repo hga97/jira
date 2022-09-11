ce5195f8c7a17c09df69bbf97991bb3bda99f832
tom <gitlab@tom.com>
2022-09-11 17:28:53 +0800

    feat: 错误边界

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import "./App.css";
 import { Authenticated } from "./authenticated-app";
 import { UnAuthenticated } from "unauthenticated-app";
 import { useAuth } from "context/auth-context";
+import { ErrorBoundary } from "components/error-Boundary";
+import { FullPageErrorFallBack } from "components/lib";
 
 function App() {
   const { user } = useAuth();
   return (
-    <div className="App">{user ? <Authenticated /> : <UnAuthenticated />}</div>
+    <div className="App">
+      <ErrorBoundary fallbackRender={FullPageErrorFallBack}>
+        {user ? <Authenticated /> : <UnAuthenticated />}
+      </ErrorBoundary>
+    </div>
   );
 }
 
